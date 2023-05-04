fb0f70a8bedf75baa86552d0680182f957eb8c2f
puutaro <hoperstep@gmail.com>
2023-05-04 11:58:36 +0900

    remove tooast debug

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/UrlHistoryBackUp.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/UrlHistoryBackUp.kt
@@ -1,6 +1,5 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment
 
-import android.widget.Toast
 import com.puutaro.commandclick.common.variable.UsePath
 import com.puutaro.commandclick.fragment.TerminalFragment
 import com.puutaro.commandclick.util.FileSystems
