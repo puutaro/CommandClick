3588c2dbb7ce2ab1ef861029147e63534d87abab
puutaro <hoperstep@gmail.com>
2023-05-09 01:35:52 +0900

    downlaod mode change

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/ExecDownLoadManager.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/ExecDownLoadManager.kt
@@ -5,13 +5,16 @@ import android.app.AlertDialog
 import android.app.DownloadManager
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.net.Uri
 import android.os.Environment
 import android.view.Gravity
 import android.webkit.DownloadListener
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.puutaro.commandclick.fragment.TerminalFragment
 import java.net.URLDecoder
+
 
 class ExecDownLoadManager {
     companion object {
@@ -23,59 +26,61 @@ class ExecDownLoadManager {
 
             binding.terminalWebView.setDownloadListener(DownloadListener {
                     url, userAgent, contentDisposition, mimetype, contentLength ->
-
-                val convetedContentDispositon = makeConvetedContentDispositon(
-                    contentDisposition
-                )
-                val isDriveDownload =
-                    !(contentDisposition == convetedContentDispositon)
-                val downloadFileName = makeDownloadFile(
-                    url,
-                    convetedContentDispositon,
-                    isDriveDownload
-                )
-
-                val downlaodUrl = makeUrl(
-                    url,
-                    terminalFragment,
-                    isDriveDownload,
-                )
-
-                val request = DownloadManager.Request(
-                    Uri.parse(downlaodUrl)
-                )
-
-                val alertDialog = AlertDialog.Builder(context)
-                    .setTitle(
-                        "download ok?"
-                    )
-                    .setMessage("file: ${downloadFileName}")
-                    .setPositiveButton("OK", DialogInterface.OnClickListener {
-                            dialog, which ->
-                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                        request.setTitle("cmdclick")
-                        request.setDescription("downloading..\n${downloadFileName}")
-                        request.setDestinationInExternalPublicDir(
-                            Environment.DIRECTORY_DOWNLOADS,
-                            downloadFileName
-                        )
-                        val dm = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
-                        dm?.enqueue(request)
-                        Toast.makeText(
-                            context,
-                            "Downloading : ${downloadFileName}\ndetail is in norificatin bar",  //To notify the Client that the file is being downloaded
-                            Toast.LENGTH_LONG
-                        ).show()
-                    })
-                    .setNegativeButton("NO", null)
-                    .show()
-                alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(
-                    context?.getColor(R.color.black) as Int
-                );
-                alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(
-                    context.getColor(R.color.black) as Int
-                )
-                alertDialog.getWindow()?.setGravity(Gravity.BOTTOM)
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                terminalFragment.startActivity(i)
+//                val convetedContentDispositon = makeConvetedContentDispositon(
+//                    contentDisposition
+//                )
+//                val isDriveDownload =
+//                    !(contentDisposition == convetedContentDispositon)
+//                val downloadFileName = makeDownloadFile(
+//                    url,
+//                    convetedContentDispositon,
+//                    isDriveDownload
+//                )
+//
+//                val downlaodUrl = makeUrl(
+//                    url,
+//                    terminalFragment,
+//                    isDriveDownload,
+//                )
+//
+//                val request = DownloadManager.Request(
+//                    Uri.parse(downlaodUrl)
+//                )
+//
+//                val alertDialog = AlertDialog.Builder(context)
+//                    .setTitle(
+//                        "download ok?"
+//                    )
+//                    .setMessage("file: ${downloadFileName}")
+//                    .setPositiveButton("OK", DialogInterface.OnClickListener {
+//                            dialog, which ->
+//                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+//                        request.setTitle("cmdclick")
+//                        request.setDescription("downloading..\n${downloadFileName}")
+//                        request.setDestinationInExternalPublicDir(
+//                            Environment.DIRECTORY_DOWNLOADS,
+//                            downloadFileName
+//                        )
+//                        val dm = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+//                        dm?.enqueue(request)
+//                        Toast.makeText(
+//                            context,
+//                            "Downloading : ${downloadFileName}\ndetail is in norificatin bar",  //To notify the Client that the file is being downloaded
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                    })
+//                    .setNegativeButton("NO", null)
+//                    .show()
+//                alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(
+//                    context?.getColor(R.color.black) as Int
+//                );
+//                alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(
+//                    context.getColor(R.color.black) as Int
+//                )
+//                alertDialog.getWindow()?.setGravity(Gravity.BOTTOM)
             })
         }
     }
