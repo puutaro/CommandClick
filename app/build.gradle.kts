b5204ae27aba43ba6fbc5537494b8028a7d498f9
puutaro <hoperstep@gmail.com>
2023-07-24 14:05:57 +0900

    support monochrome icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.puutaro.commandclick"
         minSdk = 27
         targetSdk = 33
-        versionCode = 34
+        versionCode = 35
         versionName = "${versionMajor}.${versionMinor}.${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
