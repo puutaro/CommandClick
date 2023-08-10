1ef98a10c799bdc167b563b3dd86181f8d35666a
puutaro <hoperstep@gmail.com>
2023-08-10 18:49:33 +0900

    visual update multiselect dialog

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/MultiSelectOnlyImageGridViewJsDialog.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/MultiSelectOnlyImageGridViewJsDialog.kt
@@ -1,17 +1,15 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.lib
 
-import android.app.AlertDialog
-import android.content.DialogInterface
+import android.app.Dialog
 import android.view.Gravity
-import android.widget.AbsListView
+import android.view.ViewGroup
 import android.widget.GridView
-import android.widget.LinearLayout
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.puutaro.commandclick.component.adapter.MultiSelectOnlyImageAdapter
 import com.puutaro.commandclick.fragment.TerminalFragment
-import com.puutaro.commandclick.proccess.lib.LinearLayoutForTotal
-import com.puutaro.commandclick.proccess.lib.NestLinearLayout
-import com.puutaro.commandclick.proccess.lib.SearchTextLinearWeight
 import com.puutaro.commandclick.view_model.activity.TerminalViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -24,7 +22,7 @@ class MultiSelectOnlyImageGridViewJsDialog(
     private val context = terminalFragment.context
     private val terminalViewModel: TerminalViewModel by terminalFragment.activityViewModels()
     private var returnValue = String()
-    private var alertDialog: AlertDialog? = null
+    private var gridDialogObj: Dialog? = null
 
     fun create(
         title: String,
@@ -52,111 +50,84 @@ class MultiSelectOnlyImageGridViewJsDialog(
 
     }
 
-    private fun createLinearLayoutForGridView(
-        imagePathList: List<String>,
-    ): LinearLayout {
-
-        val gridView = GridView(context)
-        gridView.numColumns = 2
-        gridView.choiceMode = AbsListView.CHOICE_MODE_MULTIPLE
-
-        val myImageAdapter = MultiSelectOnlyImageAdapter(context)
-        myImageAdapter.addAll(
-            imagePathList.toMutableList()
-        )
-        gridView.adapter = myImageAdapter
-        invokeListItemSetClickListenerForListDialog(
-            gridView,
-        )
-
-        val linearLayoutForTotal = LinearLayoutForTotal.make(
-            context
-        )
-        val searchTextWeight = SearchTextLinearWeight.calculate(terminalFragment)
-        val listWeight = 1F - searchTextWeight
-        val linearLayoutForListView = NestLinearLayout.make(
-            context,
-            listWeight
-        )
-        val linearLayoutForSearch = NestLinearLayout.make(
-            context,
-            searchTextWeight
-        )
-        linearLayoutForListView.addView(gridView)
-        linearLayoutForTotal.addView(linearLayoutForListView)
-        linearLayoutForTotal.addView(linearLayoutForSearch)
-        return linearLayoutForTotal
-    }
-
     private fun execCreate(
         title: String,
         message: String,
         imagePathListTabSepaStr: String,
     ) {
-        val imagePathList =
-            imagePathListTabSepaStr
-                .split("\t")
-                .toMutableList()
-        val context = context ?: return
-        val linearLayoutForGridView = createLinearLayoutForGridView(
-            imagePathList,
+        if(
+            context == null
+        ) return
+        gridDialogObj = Dialog(
+            context
         )
-
-        val titleString = if(
+        gridDialogObj?.setContentView(
+            com.puutaro.commandclick.R.layout.multi_select_grid_dialog_layout
+        )
+        val titleTextView = gridDialogObj?.findViewById<AppCompatTextView>(
+            com.puutaro.commandclick.R.id.multi_select_grid_dialog_title
+        ) ?: return
+        if(
             title.isNotEmpty()
-        ){
-            title
-        } else "Select bellow list"
-        terminalFragment.alertDialogInstance = if(
+        ) titleTextView.text = "Select bellow list"
+        else titleTextView.isVisible = false
+        val messageTextView = gridDialogObj?.findViewById<AppCompatTextView>(
+            com.puutaro.commandclick.R.id.multi_select_grid_dialog_message
+        ) ?: return
+        if(
             message.isNotEmpty()
-        ) {
-            AlertDialog.Builder(
-                context
-            )
-                .setTitle(titleString)
-                .setMessage(message)
-                .setView(linearLayoutForGridView)
-                .setNegativeButton("NO", DialogInterface.OnClickListener{ dialog, which ->
-                    terminalFragment.alertDialogInstance?.dismiss()
-                    terminalViewModel.onDialog = false
-                    returnValue = String()
-                })
-                .setPositiveButton("OK", DialogInterface.OnClickListener{ dialog, which ->
-                    terminalFragment.alertDialogInstance?.dismiss()
-                    terminalViewModel.onDialog = false
-                })
-                .show()
-        } else {
-            AlertDialog.Builder(
-                context,
-            )
-                .setTitle(titleString)
-                .setView(linearLayoutForGridView)
-                .setNegativeButton("NO", DialogInterface.OnClickListener{ dialog, which ->
-                    terminalFragment.alertDialogInstance?.dismiss()
-                    terminalViewModel.onDialog = false
-                    returnValue = String()
-                })
-                .setPositiveButton("OK", DialogInterface.OnClickListener{ dialog, which ->
-                    terminalFragment.alertDialogInstance?.dismiss()
-                    terminalViewModel.onDialog = false
-                })
-                .show()
+        ) messageTextView.text = message
+        else messageTextView.isVisible = false
+        setGridView(
+            imagePathListTabSepaStr
+        )
+        val cancelButton = gridDialogObj?.findViewById<ImageButton>(
+            com.puutaro.commandclick.R.id.multi_select_grid_dialog_cancel
+        ) ?: return
+        cancelButton.setOnClickListener {
+            gridDialogObj?.dismiss()
+            terminalViewModel.onDialog = false
+            returnValue = String()
         }
-        alertDialog = terminalFragment.alertDialogInstance
-        alertDialog?.window?.setGravity(Gravity.BOTTOM)
-        alertDialog?.getButton(DialogInterface.BUTTON_POSITIVE)?.setTextColor(
-            context.getColor(android.R.color.black)
+        val okButton = gridDialogObj?.findViewById<ImageButton>(
+            com.puutaro.commandclick.R.id.multi_select_grid_dialog_ok
+        ) ?: return
+        okButton.setOnClickListener {
+            gridDialogObj?.dismiss()
+            terminalViewModel.onDialog = false
+        }
+        gridDialogObj?.setOnCancelListener {
+            gridDialogObj?.dismiss()
+            terminalViewModel.onDialog = false
+        }
+        gridDialogObj?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
         )
-        alertDialog?.getButton(DialogInterface.BUTTON_NEGATIVE)?.setTextColor(
-            context.getColor(android.R.color.black)
+        gridDialogObj?.window?.setGravity(Gravity.BOTTOM)
+        gridDialogObj?.show()
+    }
+
+    private fun setGridView(
+        imagePathListTabSepaStr: String,
+    ){
+        val imagePathList = makeImagePathList(
+            imagePathListTabSepaStr
         )
-        alertDialog?.setOnCancelListener(object : DialogInterface.OnCancelListener {
-            override fun onCancel(dialog: DialogInterface?) {
-                terminalViewModel.onDialog = false
-                returnValue = String()
-            }
-        })
+        val multiSelectGridView = gridDialogObj?.findViewById<GridView>(
+            com.puutaro.commandclick.R.id.multi_select_grid_dialog_grid_view
+        ) ?: return
+        val multiSelectOnlyImageAdapter = MultiSelectOnlyImageAdapter(
+            context
+        )
+        multiSelectOnlyImageAdapter.clear()
+        multiSelectOnlyImageAdapter.addAll(
+            imagePathList.toMutableList()
+        )
+        multiSelectGridView.adapter = multiSelectOnlyImageAdapter
+        invokeListItemSetClickListenerForListDialog(
+            multiSelectGridView
+        )
     }
 
     private fun invokeListItemSetClickListenerForListDialog(
@@ -175,5 +146,12 @@ class MultiSelectOnlyImageGridViewJsDialog(
                 multiSelectOnlyImageAdapter.selectedItemList.joinToString("\t")
             return@setOnItemClickListener
         }
+    }
+
+    private fun makeImagePathList(
+        imagePathListTabSepaStr: String,
+    ): List<String> {
+        return imagePathListTabSepaStr
+                .split("\t")
     }
 }
