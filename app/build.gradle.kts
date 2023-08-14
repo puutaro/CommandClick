30ce0faff029c4f148c221960e1f4e10913409c8
puutaro <hoperstep@gmail.com>
2023-08-14 17:15:08 +0900

    change log and schreenshot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.puutaro.commandclick"
         minSdk = 27
         targetSdk = 33
-        versionCode = 48
+        versionCode = 49
         versionName = "${versionMajor}.${versionMinor}.${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
