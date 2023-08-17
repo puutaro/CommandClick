a2e6b4bb3d63fca6f92e6a30fe611ab6af14d254
puutaro <55217593+puutaro@users.noreply.github.com>
2023-08-17 18:32:10 +0900

    Create JsArgs.js

---
+++ md/js_interrface/JsArgs.js
@@ -0,0 +1,41 @@
+
+
+# JsArgs
+
+This interfce pass argment to script  
+
+
+```js.js
+	jsArgs.get()
+		-> tabsepalete string  
+			jsArgs soruce is jsf argument in edit  
+			ex) setVariableType="jsf $0 fristargment 'secondargument 2'" 
+				-> `fristargment`\t`secondargument 2`  
+
+	jsArgs.set(
+		tabsepalete string
+	    )
+		-> argment set (ex "{arg1}\t{arg2}\t..")  
+
+```
+
+
+- example 
+
+```js.js
+
+/// SETTING_SECTION_START
+setVariableTypes="buttonText:BTN=cmd=jsf '${0}' firstArgs second args!label=this"
+/// SETTING_SECTION_END
+
+
+/// CMD_VARIABLE_SECTION_START
+buttonText=""
+/// CMD_VARIABLE_SECTION_END
+
+
+let args = jsArgs.get().split("\t");
+const firstArgs = args.at(0);
+const secondArgs = args.at(0);
+
+```
