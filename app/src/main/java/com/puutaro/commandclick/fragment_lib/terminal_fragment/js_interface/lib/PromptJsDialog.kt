47cbf1a6f36268a8611a516d532f378a7d3c9ef1
puutaro <hoperstep@gmail.com>
2023-10-01 01:54:56 +0900

    add suggest off setting

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/PromptJsDialog.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/PromptJsDialog.kt
@@ -166,6 +166,7 @@ class PromptJsDialog(
         variableName: String,
     ){
         if(promptEditText == null) return
+        if(variableName.isEmpty()) return
         promptEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
