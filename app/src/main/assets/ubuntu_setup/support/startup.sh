faf4683deb1f9159b07dbc7ee9ed261378e9dd4e
puutaro <hoperstep@gmail.com>
2023-10-23 11:33:25 +0900

    modify pulse startup system

--- app/src/main/assets/ubuntu_setup/support/startup.sh
+++ app/src/main/assets/ubuntu_setup/support/startup.sh
@@ -247,6 +247,7 @@ pulseaudioSetup(){
 	rm -rf \$HOME/.config/pulse
 	echo --- pulseaudio --start
 	retry_times=5
+	ok_count = 0
 	for i in \$(seq \${retry_times})
 	do
 		shellCon="\$(curl 127.0.0.1:${UBUNTU_PC_PULSE_SET_SERVER_PORT})"
@@ -255,9 +256,13 @@ pulseaudioSetup(){
 			*)	
 				echo "UBUNTU_PC_PULSE_SET_SERVER_PORT ${UBUNTU_PC_PULSE_SET_SERVER_PORT}"
 				sh -c "\${shellCon}"
-				break
+				ok_count=\$((ok_count + 1))
+				echo "ok_count: \${ok_count}"
 				;;
 		esac
+		if [ -n "\${shellCon}" ] && [ \${ok_count} -ge 2 ]; then
+			break;
+		fi
 		echo  "[\${i}/\${retry_times}] re-try pulseaudio --start"
 		sleep 2
 	done
