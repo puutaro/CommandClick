5076c524d479882a8043a8c80d97c8345ab689ac
puutaro <hoperstep@gmail.com>
2023-10-24 21:27:43 +0900

    change text to speech args

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/JsTextToSpeech.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/JsTextToSpeech.kt
@@ -1,16 +1,11 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface
 
-import android.content.Context
 import android.content.Intent
 import android.speech.tts.TextToSpeech
 import android.webkit.JavascriptInterface
-import android.widget.Toast
-import com.puutaro.commandclick.common.variable.settings.SharePrefferenceSetting
-import com.puutaro.commandclick.common.variable.intent.TextToSpeechIntentExtra
 import com.puutaro.commandclick.fragment.TerminalFragment
+import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.lib.TextToSpeechIntentSender
 import com.puutaro.commandclick.service.TextToSpeechService
-import com.puutaro.commandclick.util.SharePreffrenceMethod
-import java.io.File
 
 class JsTextToSpeech(
     private val terminalFragment: TerminalFragment,
@@ -20,48 +15,13 @@ class JsTextToSpeech(
     @JavascriptInterface
     fun speech(
         listFilePath: String,
-        playMode: String,
-        onRoop: String,
-        playNumber: String,
-        transMode: String,
-        onTrack: String,
-        speed: String,
-        pitch: String
+        extraSettingMapStr: String,
     ) {
-        try {
-            val sharePref = terminalFragment.activity?.getPreferences(Context.MODE_PRIVATE)
-            val currentAppDirName = SharePreffrenceMethod.getStringFromSharePreffrence(
-                sharePref,
-                SharePrefferenceSetting.current_app_dir
-            ).let {
-                File(it).name
-            }
-            val fannelRawName = SharePreffrenceMethod.getStringFromSharePreffrence(
-                sharePref,
-                SharePrefferenceSetting.current_script_file_name
-            ).replace(
-                Regex("\\.[a-zA-Z0-9]*$"),
-                ""
-            )
-            val intent = Intent(terminalFragment.activity, TextToSpeechService::class.java)
-            intent.putExtra(TextToSpeechIntentExtra.listFilePath.scheme, listFilePath)
-            intent.putExtra(TextToSpeechIntentExtra.playMode.scheme, playMode)
-            intent.putExtra(TextToSpeechIntentExtra.onRoop.scheme, onRoop)
-            intent.putExtra(TextToSpeechIntentExtra.playNumber.scheme, playNumber)
-            intent.putExtra(TextToSpeechIntentExtra.transMode.scheme, transMode)
-            intent.putExtra(TextToSpeechIntentExtra.onTrack.scheme, onTrack)
-            intent.putExtra(TextToSpeechIntentExtra.speed.scheme, speed)
-            intent.putExtra(TextToSpeechIntentExtra.pitch.scheme, pitch)
-            intent.putExtra(TextToSpeechIntentExtra.currentAppDirName.scheme, currentAppDirName)
-            intent.putExtra(TextToSpeechIntentExtra.scriptRawName.scheme, fannelRawName)
-            context?.startForegroundService(intent)
-        } catch (e: Exception) {
-            Toast.makeText(
-                terminalFragment.context,
-                e.toString(),
-                Toast.LENGTH_LONG
-            ).show()
-        }
+        TextToSpeechIntentSender.send(
+            terminalFragment,
+            listFilePath,
+            extraSettingMapStr,
+        )
     }
 
     @JavascriptInterface
--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/TextToSpeechIntentSender.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/lib/TextToSpeechIntentSender.kt
@@ -1,4 +1,143 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.lib
 
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.puutaro.commandclick.common.variable.intent.TextToSpeechIntentExtra
+import com.puutaro.commandclick.common.variable.settings.SharePrefferenceSetting
+import com.puutaro.commandclick.fragment.TerminalFragment
+import com.puutaro.commandclick.service.TextToSpeechService
+import com.puutaro.commandclick.util.CmdClickMap
+import com.puutaro.commandclick.util.SharePreffrenceMethod
+import java.io.File
+
 object TextToSpeechIntentSender {
+
+    private val keySeparator = "|"
+
+    fun send(
+        terminalFragment: TerminalFragment,
+        listFilePath: String,
+        extraSettingMapStr: String,
+    ){
+        try {
+            exeSend(
+                terminalFragment,
+                listFilePath,
+                extraSettingMapStr,
+            )
+        }catch (e: Exception){
+            Toast.makeText(
+                terminalFragment.context,
+                e.toString(),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun exeSend(
+        terminalFragment: TerminalFragment,
+        listFilePath: String,
+        extraSettingMapStr: String,
+    ){
+        val sharePref = terminalFragment.activity?.getPreferences(Context.MODE_PRIVATE)
+        val currentAppDirName = SharePreffrenceMethod.getStringFromSharePreffrence(
+            sharePref,
+            SharePrefferenceSetting.current_app_dir
+        ).let {
+            File(it).name
+        }
+        val fannelRawName = SharePreffrenceMethod.getStringFromSharePreffrence(
+            sharePref,
+            SharePrefferenceSetting.current_script_file_name
+        ).replace(
+            Regex("\\.[a-zA-Z0-9]*$"),
+            ""
+        )
+        val textToSpeechIntent = Intent(terminalFragment.activity, TextToSpeechService::class.java)
+        val extraSettingMap = CmdClickMap.createMap(
+            extraSettingMapStr,
+            keySeparator
+        ).toMap()
+
+        textToSpeechIntent.putExtra(
+            TextToSpeechIntentExtra.listFilePath.scheme,
+            listFilePath
+        )
+        extraSettingMap.get(TextToSpeechShema.playMode.name).let {
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.playMode.scheme,
+                it
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.onRoop.name).let {
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.onRoop.scheme,
+                it
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.playNumber.name).let {
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.playNumber.scheme,
+                it
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.transMode.name).let {
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.transMode.scheme,
+                it
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.onTrack.name).let {
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.onTrack.scheme,
+                it
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.speed.name)?.let {
+            val floatSpeed = toFloatStr(it)
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.speed.scheme,
+                floatSpeed
+            )
+        }
+        extraSettingMap.get(TextToSpeechShema.pitch.name)?.let {
+            val floatPitch = toFloatStr(it)
+            textToSpeechIntent.putExtra(
+                TextToSpeechIntentExtra.pitch.scheme,
+                floatPitch
+            )
+        }
+        textToSpeechIntent.putExtra(
+            TextToSpeechIntentExtra.currentAppDirName.scheme,
+            currentAppDirName
+        )
+        textToSpeechIntent.putExtra(
+            TextToSpeechIntentExtra.scriptRawName.scheme,
+            fannelRawName
+        )
+        terminalFragment.context?.startForegroundService(textToSpeechIntent)
+    }
+
+    private fun toFloatStr(
+        intStr: String
+    ): String {
+        return try {
+            val floatInt = intStr.toFloat()
+            if(floatInt > 1000) return "1000"
+            (intStr.toFloat() / 50).toString()
+        } catch (e: Exception){
+            "1"
+        }
+    }
+}
+
+private enum class TextToSpeechShema {
+    playMode,
+    onRoop,
+    playNumber,
+    transMode,
+    onTrack,
+    speed,
+    pitch,
 }
