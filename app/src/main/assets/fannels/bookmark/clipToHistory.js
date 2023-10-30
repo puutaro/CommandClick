5f2548efdb79025b5dc2a4783e3e379de66e30d8
puutaro <hoperstep@gmail.com>
2023-10-30 15:49:29 +0900

    add clipToHistory.js

---
+++ app/src/main/assets/fannels/bookmark/clipToHistory.js
@@ -0,0 +1,24 @@
+
+
+clipToHistory();
+
+function clipToHistory(){
+	const clipUrl = jsUtil.echoFromClipboard();
+	if(!clipUrl) return;
+	if(
+		!clipUrl.startsWith('http://')
+		&& !clipUrl.startsWith('https://')
+	) return;
+	const currentAppDirPath = "${01}".replace("/js", "");
+	const historyPath = `${currentAppDirPath}/url/cmdclickUrlHistory.tsv`;
+	const currentHistoryCon = jsFileSystem.readLocalFile(
+		historyPath
+	);
+	const newLine = `${clipUrl}\t${clipUrl}`;
+	const saveHisCon = `${newLine}\n${currentHistoryCon}`;
+	jsFileSystem.writeLocalFile(
+		historyPath,
+		saveHisCon
+	);
+	location.reload();
+};
