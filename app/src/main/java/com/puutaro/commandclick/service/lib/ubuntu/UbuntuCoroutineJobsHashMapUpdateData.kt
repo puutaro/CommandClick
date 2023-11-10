d6210293d00982eb06a1dd0f075a3793a912990d
puutaro <hoperstep@gmail.com>
2023-11-10 11:30:09 +0900

    remove log

--- app/src/main/java/com/puutaro/commandclick/service/lib/ubuntu/UbuntuCoroutineJobsHashMapUpdateData.kt
+++
@@ -1,8 +0,0 @@
-package com.puutaro.commandclick.service.lib.ubuntu
-
-import kotlinx.coroutines.Job
-
-data class UbuntuCoroutineJobsHashMapUpdateData(
-    val ubuntuCoroutineJobsHashMap: HashMap<String, Job?>,
-    val backgroundJobType: String,
-)
