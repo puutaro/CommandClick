6eb3095f3bc383e2a34705c96e1c778052f6eae9
puutaro <hoperstep@gmail.com>
2023-11-28 12:28:08 +0900

    download bug fix

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/ExecDownLoadManager.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/ExecDownLoadManager.kt
@@ -1,5 +1,7 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.webkit.DownloadListener
@@ -16,14 +18,48 @@ object ExecDownLoadManager {
         val context = terminalFragment.context
         webView.setDownloadListener(DownloadListener {
                 url, userAgent, contentDisposition, mimetype, contentLength ->
+            try {
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                terminalFragment.startActivity(i)
+                Toast.makeText(
+                    context,
+                    "Select download browser",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } catch(e: Exception){
+                openChorme(
+                    context,
+                    webView.url,
+                )
+            }
+        })
+    }
+
+    private fun openChorme(
+        context: Context?,
+        urlString: String?,
+    ){
+        if(
+            context == null
+        ) return
+        if(
+            urlString.isNullOrEmpty()
+        ) return
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setPackage("com.android.chrome")
+        try {
+            context.startActivity(intent)
             Toast.makeText(
                 context,
-                "Select download browser",
+                "Select download link by chrome",
                 Toast.LENGTH_SHORT
             ).show()
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            terminalFragment.startActivity(i)
-        })
+        } catch (ex: ActivityNotFoundException) {
+            // Chrome browser presumably not installed so allow user to choose instead
+            intent.setPackage(null)
+            context.startActivity(intent)
+        }
     }
 }
