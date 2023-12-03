75ffef77eb7ce8acfad35b56742c511f48102949
puutaro <hoperstep@gmail.com>
2023-12-03 19:30:42 +0900

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.puutaro.commandclick"
         minSdk = 27
         targetSdk = 33
-        versionCode = 13
+        versionCode = 14
         versionName = "${versionMajor}.${versionMinor}.${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
