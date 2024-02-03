f6b3cc526337b311988c8620c634535b19df2772
puutaro <hoperstep@gmail.com>
2024-02-03 17:15:31 +0900

    add add-app dire macro guard

--- app/src/main/java/com/puutaro/commandclick/proccess/list_index_for_edit/libs/js_path_handler_for_list_index/ExecRenameAppDir.kt
+++ app/src/main/java/com/puutaro/commandclick/proccess/list_index_for_edit/libs/js_path_handler_for_list_index/ExecRenameAppDir.kt
@@ -12,7 +12,9 @@ import com.puutaro.commandclick.R
 import com.puutaro.commandclick.common.variable.path.UsePath
 import com.puutaro.commandclick.common.variable.variables.CommandClickScriptVariable
 import com.puutaro.commandclick.component.adapter.ListIndexForEditAdapter
+import com.puutaro.commandclick.proccess.list_index_for_edit.ListIndexEditConfig
 import com.puutaro.commandclick.proccess.list_index_for_edit.config_settings.ListSettingsForListIndex
+import com.puutaro.commandclick.proccess.list_index_for_edit.config_settings.TypeSettingsForListIndex
 import com.puutaro.commandclick.proccess.list_index_for_edit.libs.ListIndexArgsMaker
 import com.puutaro.commandclick.util.CcPathTool
 import com.puutaro.commandclick.util.file.FileSystems
@@ -26,6 +28,16 @@ object ExecRenameAppDir {
         selectedItem: String,
     ){
         val editFragment = listIndexArgsMaker.editFragment
+        val type = ListIndexEditConfig.getListIndexType(
+            editFragment
+        )
+        when(type){
+            TypeSettingsForListIndex.ListIndexTypeKey.INSTALL_FANNEL,
+            TypeSettingsForListIndex.ListIndexTypeKey.TSV_EDIT
+            -> return
+            TypeSettingsForListIndex.ListIndexTypeKey.NORMAL
+            -> {}
+        }
         execRenameForAppDirAdmin(
             listIndexArgsMaker,
             selectedItem,
