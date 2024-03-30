2d16097a04fcbe0c1e18c75cd93d8cfa841ead6a
puutaro <hoperstep@gmail.com>
2024-03-31 02:36:18 +0900

    add get share pref js intarface

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/system/JsSharePref.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/system/JsSharePref.kt
@@ -1,4 +1,32 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.system
 
-class JsSharePref {
+import android.webkit.JavascriptInterface
+import com.puutaro.commandclick.fragment.TerminalFragment
+import com.puutaro.commandclick.util.state.SharePrefTool
+
+class JsSharePref(
+    terminalFragment: TerminalFragment
+) {
+    val readSharePreferenceMap = terminalFragment.readSharePreferenceMap
+
+    @JavascriptInterface
+    fun getFannelName(): String {
+        return SharePrefTool.getCurrentFannelName(
+            readSharePreferenceMap
+        )
+    }
+
+    @JavascriptInterface
+    fun getAppDirPath(): String {
+        return SharePrefTool.getCurrentAppDirPath(
+            readSharePreferenceMap
+        )
+    }
+
+    @JavascriptInterface
+    fun getState(): String {
+        return SharePrefTool.getCurrentStateName(
+            readSharePreferenceMap
+        )
+    }
 }
