cb3ed0bdf559fb261b1c162fcfef2e26741fde55
puutaro <hoperstep@gmail.com>
2024-03-31 02:33:50 +0900

    add js func to reflect before info to cur info

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/judge/JsBeforeInfo.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/judge/JsBeforeInfo.kt
@@ -1,4 +1,44 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.judge
 
-class JsBeforeInfo {
+import android.webkit.JavascriptInterface
+import com.puutaro.commandclick.fragment.TerminalFragment
+import com.puutaro.commandclick.util.file.FileSystems
+import com.puutaro.commandclick.util.file.ReadText
+
+class JsBeforeInfo(
+    terminalFragment: TerminalFragment
+) {
+    @JavascriptInterface
+    fun match(
+        beforeInfoPath: String,
+        curInfo: String,
+    ): Boolean {
+        val beforeInfo = ReadText(
+            beforeInfoPath
+        ).readText()
+        val isEqual =
+            beforeInfo == curInfo
+        FileSystems.writeFile(
+            beforeInfoPath,
+            curInfo
+        )
+        return isEqual
+    }
+
+    @JavascriptInterface
+    fun misMatch(
+        beforeInfoPath: String,
+        curInfo: String,
+    ): Boolean {
+        val beforeInfo = ReadText(
+            beforeInfoPath
+        ).readText()
+        val isEqual =
+            beforeInfo != curInfo
+        FileSystems.writeFile(
+            beforeInfoPath,
+            curInfo
+        )
+        return isEqual
+    }
 }
