9c70cc3785df3a76f21664d00ccaff8f6ae7dfd0
puutaro <hoperstep@gmail.com>
2024-04-06 15:57:50 +0900

    add color and icon

--- app/src/main/java/com/puutaro/commandclick/common/variable/res/CmdClickColor.kt
+++ app/src/main/java/com/puutaro/commandclick/common/variable/res/CmdClickColor.kt
@@ -10,5 +10,8 @@ enum class CmdClickColor(
     GRAY("gray", R.color.gray_out),
     LIGHT_GREEN("lightGreen", R.color.fannel_icon_color),
     DARK_GREEN("darkGreen", R.color.terminal_color),
-    RED("red", com.termux.shared.R.color.dark_red)
+    RED("red", com.termux.shared.R.color.dark_red),
+    WHITE("white", R.color.white),
+    YELLOW("yellow", R.color.yellow),
+    BLACK("black", R.color.black),
 }
