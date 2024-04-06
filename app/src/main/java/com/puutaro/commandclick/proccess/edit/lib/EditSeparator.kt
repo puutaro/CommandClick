3a02a3de51a80f5d4c5251526611c6c1efc24d5f
puutaro <hoperstep@gmail.com>
2024-04-07 01:16:17 +0900

    add edit separator manager

--- app/src/main/java/com/puutaro/commandclick/proccess/edit/lib/EditSeparator.kt
+++ app/src/main/java/com/puutaro/commandclick/proccess/edit/lib/EditSeparator.kt
@@ -1,4 +1,17 @@
 package com.puutaro.commandclick.proccess.edit.lib
 
 object EditSeparator {
+
+    private val separatorOrderList = listOf(
+        '|', '?', '&'
+    )
+
+    fun getNextSeparator(
+        currentSeparator: Char
+    ): Char? {
+        val currentSeparatorIndex =
+            separatorOrderList.indexOf(currentSeparator)
+        val nextSeparatorIndex = currentSeparatorIndex + 1
+        return separatorOrderList.getOrNull(nextSeparatorIndex)
+    }
 }
