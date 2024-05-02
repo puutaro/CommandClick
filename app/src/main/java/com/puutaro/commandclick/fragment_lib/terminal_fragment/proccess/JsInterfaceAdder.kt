f74e7f609771f6adea7fffaa79c334bf06c53aa4
puutaro <hoperstep@gmail.com>
2024-05-03 03:26:07 +0900

    add js state changer

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/proccess/JsInterfaceAdder.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/proccess/JsInterfaceAdder.kt
@@ -74,6 +74,7 @@ import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.syst
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.system.JsKeyboard
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.system.JsMonitorShower
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.system.JsSharePref
+import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.system.JsStateChange
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.toolbar.JsAddGmailCon
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.toolbar.JsAddToUrlHistory
 import com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.toolbar.JsAddUrlCon
@@ -404,6 +405,10 @@ object JsInterfaceAdder {
         ExecJsInterfaceAdder.add(
             webView,
             JsKeyboard(terminalFragment)
+        )
+        ExecJsInterfaceAdder.add(
+            webView,
+            JsStateChange(terminalFragment)
         )
     }
 
