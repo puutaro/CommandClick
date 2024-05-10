78242b4ffc46ce89b61f14d699b73969828118f4
puutaro <hoperstep@gmail.com>
2024-05-11 01:47:32 +0900

    bug fix (item click js path not woriking)

--- app/src/main/assets/appSystemDir/system/jsImportManagerDir/settings/listIndexConfig.js
+++ app/src/main/assets/appSystemDir/system/jsImportManagerDir/settings/listIndexConfig.js
@@ -9,7 +9,7 @@ longClick=
     ?args=
         menuPath=`${JS_IMPORT_LONG_PRESS_MENU_PATH}`,
 click=
-    func=`${JS_IMPORT_SHOW_CON_JS_PATH}`
+    jsPath=`${JS_IMPORT_SHOW_CON_JS_PATH}`
     ?args=
         "INDEX_LIST_DIR_PATH=${INDEX_LIST_DIR_PATH}|ITEM_NAME=${ITEM_NAME}"
     |enableUpdate=ON,
