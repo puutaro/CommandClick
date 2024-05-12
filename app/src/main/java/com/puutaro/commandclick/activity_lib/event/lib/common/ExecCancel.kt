0437b7b89a64c1015fd8df4e2ae9e0c80ccfc0e2
puutaro <hoperstep@gmail.com>
2024-05-13 03:39:51 +0900

    class -> object

--- app/src/main/java/com/puutaro/commandclick/activity_lib/event/lib/common/ExecCancel.kt
+++ app/src/main/java/com/puutaro/commandclick/activity_lib/event/lib/common/ExecCancel.kt
@@ -4,20 +4,18 @@ import com.puutaro.commandclick.R
 import com.puutaro.commandclick.activity.MainActivity
 import com.puutaro.commandclick.activity_lib.manager.WrapFragmentManager
 
-class ExecCancel {
-    companion object {
-        fun execCancel(
-            activity: MainActivity,
-        ) {
+object ExecCancel {
+    fun execCancel(
+        activity: MainActivity,
+    ) {
 
-            val context = activity.applicationContext
-            val supportFragmentManagerInctance = activity.supportFragmentManager
+        val context = activity.applicationContext
+        val supportFragmentManagerInctance = activity.supportFragmentManager
 
-            WrapFragmentManager.changeFragmentAtCancelClickWhenConfirm(
-                supportFragmentManagerInctance,
-                context.getString(R.string.index_terminal_fragment),
-                context.getString(R.string.command_index_fragment),
-            )
-        }
+        WrapFragmentManager.changeFragmentAtCancelClickWhenConfirm(
+            supportFragmentManagerInctance,
+            context.getString(R.string.index_terminal_fragment),
+            context.getString(R.string.command_index_fragment),
+        )
     }
 }
