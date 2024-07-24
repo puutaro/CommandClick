e272531d7a3b298f44fb18899025902ab2437cfa
puutaro <hoperstep@gmail.com>
2024-07-24 20:25:29 +0900

    add text editor js interface

--- app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/edit/JsEditor.kt
+++ app/src/main/java/com/puutaro/commandclick/fragment_lib/terminal_fragment/js_interface/edit/JsEditor.kt
@@ -1,14 +1,22 @@
 package com.puutaro.commandclick.fragment_lib.terminal_fragment.js_interface.edit
 
 import android.webkit.JavascriptInterface
+import com.blankj.utilcode.util.ToastUtils
 import com.puutaro.commandclick.fragment.TerminalFragment
+import com.puutaro.commandclick.util.editor.EditorByEditText
 import com.puutaro.commandclick.util.editor.EditorByIntent
+import com.puutaro.commandclick.util.file.ReadText
+import com.puutaro.commandclick.util.state.FannelInfoTool
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 class JsEditor(
-    terminalFragment: TerminalFragment
+    private val terminalFragment: TerminalFragment
 ) {
     val context = terminalFragment.context
+
     @JavascriptInterface
     fun open_S(
         filePath: String,
@@ -22,5 +30,31 @@ class JsEditor(
             fileName,
             context
         ).byIntent()
+    }
+
+    @JavascriptInterface
+    fun byEditText(
+        textPath: String,
+    ){
+        val textPathObj = File(textPath)
+        val parentDirPath = textPathObj.parent
+            ?: return
+        val fileName = textPathObj.name
+        val editCon = ReadText(
+            textPath
+        ).readText()
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                EditorByEditText.byEditText(
+                    terminalFragment,
+                    parentDirPath,
+                    fileName,
+                    editCon,
+                    null
+                )
+            } catch (e: Exception) {
+                ToastUtils.showLong(e.toString())
+            }
+        }
     }
 }
