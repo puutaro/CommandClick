5c4c0cc2e8c183328a4627c8f61ea204c6c7463b
puutaro <hoperstep@gmail.com>
2024-10-02 02:40:07 +0900

    update

--- .github/workflows/shell/count_download.sh
+++ .github/workflows/shell/count_download.sh
@@ -18,116 +18,116 @@ mkdir -p "${STATISTICS_DIR_PATH}"
 readonly CC_DOWNLOAD_COUNT_DIR_PATH="${STATISTICS_DIR_PATH}/download_count"
 mkdir -p "${CC_DOWNLOAD_COUNT_DIR_PATH}"
 
-res="$(\
-    echo '{
-      "url": "https://api.github.com/repos/puutaro/repbash/releases/164539090",
-      "assets_url": "https://api.github.com/repos/puutaro/repbash/releases/164539090/assets",
-      "upload_url": "https://uploads.github.com/repos/puutaro/repbash/releases/164539090/assets{?name,label}",
-      "html_url": "https://github.com/puutaro/repbash/releases/tag/0.0.1",
-      "id": 164539090,
-      "author": {
-        "login": "github-actions[bot]",
-        "id": 41898282,
-        "node_id": "MDM6Qm90NDE4OTgyODI=",
-        "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/github-actions%5Bbot%5D",
-        "html_url": "https://github.com/apps/github-actions",
-        "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
-        "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
-        "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
-        "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
-        "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
-        "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
-        "type": "Bot",
-        "site_admin": false
-      },
-      "node_id": "RE_kwDOK87hs84JzqrS",
-      "tag_name": "0.0.1",
-      "target_commitish": "master",
-      "name": "repbash-0.0.1",
-      "draft": false,
-      "prerelease": false,
-      "created_at": "2023-12-23T09:12:07Z",
-      "published_at": "2024-07-09T05:00:44Z",
-      "assets": [
-        {
-          "url": "https://api.github.com/repos/puutaro/repbash/releases/assets/178487173",
-          "id": 178487173,
-          "node_id": "RA_kwDOK87hs84Ko3-F",
-          "name": "repbash-0.0.1-amd64",
-          "label": "",
-          "uploader": {
-            "login": "github-actions[bot]",
-            "id": 41898282,
-            "node_id": "MDM6Qm90NDE4OTgyODI=",
-            "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
-            "gravatar_id": "",
-            "url": "https://api.github.com/users/github-actions%5Bbot%5D",
-            "html_url": "https://github.com/apps/github-actions",
-            "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
-            "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
-            "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
-            "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
-            "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
-            "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
-            "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
-            "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
-            "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
-            "type": "Bot",
-            "site_admin": false
-          },
-          "content_type": "application/octet-stream",
-          "state": "uploaded",
-          "size": 7367989,
-          "download_count": 4,
-          "created_at": "2024-07-09T05:00:43Z",
-          "updated_at": "2024-07-09T05:00:44Z",
-          "browser_download_url": "https://github.com/puutaro/repbash/releases/download/0.0.1/repbash-0.0.1-amd64"
-        },
-        {
-          "url": "https://api.github.com/repos/puutaro/repbash/releases/assets/178487174",
-          "id": 178487174,
-          "node_id": "RA_kwDOK87hs84Ko3-G",
-          "name": "repbash-0.0.1-arm64",
-          "label": "",
-          "uploader": {
-            "login": "github-actions[bot]",
-            "id": 41898282,
-            "node_id": "MDM6Qm90NDE4OTgyODI=",
-            "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
-            "gravatar_id": "",
-            "url": "https://api.github.com/users/github-actions%5Bbot%5D",
-            "html_url": "https://github.com/apps/github-actions",
-            "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
-            "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
-            "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
-            "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
-            "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
-            "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
-            "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
-            "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
-            "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
-            "type": "Bot",
-            "site_admin": false
-          },
-          "content_type": "application/octet-stream",
-          "state": "uploaded",
-          "size": 7095857,
-          "download_count": 640,
-          "created_at": "2024-07-09T05:00:43Z",
-          "updated_at": "2024-07-09T05:00:43Z",
-          "browser_download_url": "https://github.com/puutaro/repbash/releases/download/0.0.1/repbash-0.0.1-arm64"
-        }
-      ],
-      "tarball_url": "https://api.github.com/repos/puutaro/repbash/tarball/0.0.1",
-      "zipball_url": "https://api.github.com/repos/puutaro/repbash/zipball/0.0.1",
-      "body": "update release"
-    }'\
-)"
+# res="$(\
+#     echo '{
+#       "url": "https://api.github.com/repos/puutaro/repbash/releases/164539090",
+#       "assets_url": "https://api.github.com/repos/puutaro/repbash/releases/164539090/assets",
+#       "upload_url": "https://uploads.github.com/repos/puutaro/repbash/releases/164539090/assets{?name,label}",
+#       "html_url": "https://github.com/puutaro/repbash/releases/tag/0.0.1",
+#       "id": 164539090,
+#       "author": {
+#         "login": "github-actions[bot]",
+#         "id": 41898282,
+#         "node_id": "MDM6Qm90NDE4OTgyODI=",
+#         "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+#         "gravatar_id": "",
+#         "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+#         "html_url": "https://github.com/apps/github-actions",
+#         "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+#         "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+#         "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+#         "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+#         "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+#         "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+#         "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+#         "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+#         "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+#         "type": "Bot",
+#         "site_admin": false
+#       },
+#       "node_id": "RE_kwDOK87hs84JzqrS",
+#       "tag_name": "0.0.1",
+#       "target_commitish": "master",
+#       "name": "repbash-0.0.1",
+#       "draft": false,
+#       "prerelease": false,
+#       "created_at": "2023-12-23T09:12:07Z",
+#       "published_at": "2024-07-09T05:00:44Z",
+#       "assets": [
+#         {
+#           "url": "https://api.github.com/repos/puutaro/repbash/releases/assets/178487173",
+#           "id": 178487173,
+#           "node_id": "RA_kwDOK87hs84Ko3-F",
+#           "name": "repbash-0.0.1-amd64",
+#           "label": "",
+#           "uploader": {
+#             "login": "github-actions[bot]",
+#             "id": 41898282,
+#             "node_id": "MDM6Qm90NDE4OTgyODI=",
+#             "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+#             "gravatar_id": "",
+#             "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+#             "html_url": "https://github.com/apps/github-actions",
+#             "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+#             "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+#             "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+#             "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+#             "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+#             "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+#             "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+#             "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+#             "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+#             "type": "Bot",
+#             "site_admin": false
+#           },
+#           "content_type": "application/octet-stream",
+#           "state": "uploaded",
+#           "size": 7367989,
+#           "download_count": 4,
+#           "created_at": "2024-07-09T05:00:43Z",
+#           "updated_at": "2024-07-09T05:00:44Z",
+#           "browser_download_url": "https://github.com/puutaro/repbash/releases/download/0.0.1/repbash-0.0.1-amd64"
+#         },
+#         {
+#           "url": "https://api.github.com/repos/puutaro/repbash/releases/assets/178487174",
+#           "id": 178487174,
+#           "node_id": "RA_kwDOK87hs84Ko3-G",
+#           "name": "repbash-0.0.1-arm64",
+#           "label": "",
+#           "uploader": {
+#             "login": "github-actions[bot]",
+#             "id": 41898282,
+#             "node_id": "MDM6Qm90NDE4OTgyODI=",
+#             "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+#             "gravatar_id": "",
+#             "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+#             "html_url": "https://github.com/apps/github-actions",
+#             "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+#             "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+#             "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+#             "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+#             "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+#             "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+#             "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+#             "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+#             "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+#             "type": "Bot",
+#             "site_admin": false
+#           },
+#           "content_type": "application/octet-stream",
+#           "state": "uploaded",
+#           "size": 7095857,
+#           "download_count": 640,
+#           "created_at": "2024-07-09T05:00:43Z",
+#           "updated_at": "2024-07-09T05:00:43Z",
+#           "browser_download_url": "https://github.com/puutaro/repbash/releases/download/0.0.1/repbash-0.0.1-arm64"
+#         }
+#       ],
+#       "tarball_url": "https://api.github.com/repos/puutaro/repbash/tarball/0.0.1",
+#       "zipball_url": "https://api.github.com/repos/puutaro/repbash/zipball/0.0.1",
+#       "body": "update release"
+#     }'\
+# )"
 
 exec_count_download(){
     local version="${1}"
@@ -182,9 +182,6 @@ count_download(){
     local name_to_donwload_count_list=(\
         $(echo "${name_to_donwload_count_list_con}")\
     )
-    # echo "gen ${name_to_donwload_count_list}"
-    # echo "list ${name_to_donwload_count_list[@]}"
-    # echo "${#name_to_donwload_count_list[@]}"
     IFS="${ifs_bk}"
     local version="$(\
         echo "${release_res}" \
