ffbf0185148ccb6cb34b6e4c0050ad0ec1825113
puutaro <hoperstep@gmail.com>
2025-01-30 11:18:02 +0900

    upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("com.android.library") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 //    id("com.chaquo.python") version "15.0.0" apply false
 }
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+//        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+//        kotlinOptions.jvmTarget = "17"
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_17.toString()
+        }
+
     }
 }
