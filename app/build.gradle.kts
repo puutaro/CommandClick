7bd64261f825102fffa81b8f34bc3b7863dab7a2
puutaro <hoperstep@gmail.com>
2025-05-14 22:36:04 +0900

    update gradele

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,20 +104,23 @@ extra["libDir"] = "${project.projectDir}/src/main/jniLibs"
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
 
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.test:core-ktx:1.6.1")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 //    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
     implementation("androidx.media:media:1.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
+//    {
+//        exclude("androidx.coordinatorlayout")
+//    }
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -156,7 +159,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 //    implementation("io.ktor:ktor-server-netty:1.4.3")
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.github.gleb8k:Android-PathShapeView:1.3.2")
     implementation("com.daimajia.androidanimations:library:2.4@aar")
