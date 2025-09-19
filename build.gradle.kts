4fd2791448ed06e2d2e16ae26828fc785a52ef40
puutaro <hoperstep@gmail.com>
2025-09-19 18:38:38 +0900

    upgrade gce

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("com.android.application") version "8.12.3" apply false
+    id("com.android.library") version "8.12.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 //    id("com.chaquo.python") version "15.0.0" apply false
 }
 
 subprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-//        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-//        kotlinOptions.jvmTarget = "17"
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
+    afterEvaluate {
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
         }
-
     }
 }
